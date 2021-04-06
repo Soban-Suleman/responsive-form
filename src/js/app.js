745a672ff53eede17dd948a260b904c0f5579710
Muhammad Soban <msoban54@gmail.com>
2021-04-06 20:39:53 +0500

    icon click defined

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,6 @@
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
-
 sign_up_btn.addEventListener("click", () => {
   container.classList.add("sign-up-mode");
 });
@@ -9,3 +8,10 @@ sign_up_btn.addEventListener("click", () => {
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
+const icons = document.querySelectorAll(".icon-clk");
+const unames = document.querySelectorAll(".focus");
+for (let i = 0; i < icons.length; i++) {
+  icons[i].addEventListener("click", () => {
+    unames[i].focus();
+  });
+}
